e8124256aef498d0fa229d161bd90e230ef84f21
cooper <cooperfu@tencent.com>
2025-04-28 17:09:15 +0800

    kafkareceiver: add sarama config[ChannelBufferSize/MaxProcessingTime] to kafkareceiver

--- receiver/kafkareceiver/kafka_receiver.go
+++ receiver/kafkareceiver/kafka_receiver.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"strconv"
 	"sync"
+	"time"
 
 	"github.com/IBM/sarama"
 	"go.opentelemetry.io/collector/component"
@@ -157,6 +158,8 @@ func createKafkaClient(config Config) (sarama.ConsumerGroup, error) {
 	saramaConfig.Consumer.Fetch.Min = config.MinFetchSize
 	saramaConfig.Consumer.Fetch.Default = config.DefaultFetchSize
 	saramaConfig.Consumer.Fetch.Max = config.MaxFetchSize
+	saramaConfig.ChannelBufferSize = config.ChannelBufferSize
+	saramaConfig.Consumer.MaxProcessingTime = config.MaxProcessingTime * time.Millisecond
 
 	var err error
 	if saramaConfig.Consumer.Offsets.Initial, err = toSaramaInitialOffset(config.InitialOffset); err != nil {
